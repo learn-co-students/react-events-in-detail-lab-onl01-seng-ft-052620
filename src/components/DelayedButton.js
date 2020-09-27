96b13be9fc70bcb95105096add77e51d5f375cda
Eyeruz <3yerus@gmail.com>
2020-09-26 20:53:29 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -6,6 +6,7 @@ class DelayedButton extends React.Component {
 
 clickEvent = (e) => {
     console.log("i got clicked")
+    e.persist()
     setTimeout(() => {
         this.props.onDelayedClick(e)
     }, this.props.delay);
